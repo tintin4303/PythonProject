4c155f2bb837c3b40412b7d0e4c92bbf16bad6da
Nyunt Tin <nyunt.workuse@gmail.com>
2025-01-21 23:12:17 +0700

    updated Rock paper scissor

--- D4/D4_RockPaperScissor.py
+++ D4/D4_RockPaperScissor.py
@@ -27,40 +27,39 @@ List = ['''
 
 print("Welcome to Rock, Paper, Scissor.")
 player = int(input("What do you choose? 0 for Rock, 1 for Paper or 2 for Scissors: "))
-while 0 <= player <= 2:
-    print(List[player])
-    opponent = random.randint(0,2)
-    print("Opponent\n", List[opponent])
+while True:
+    if 0 <= player <= 2:
+        print(List[player])
+        opponent = random.randint(0,2)
+        print("Opponent\n", List[opponent])
 
-    if player == 0:
-        if opponent == 1:
-            print("You lose.")
-        elif opponent == 2:
-            print("You win!")
+        if player == 0:
+            if opponent == 1:
+                print("You lose.")
+            elif opponent == 2:
+                print("You win!")
+            else:
+                print("Tie!")
+        elif player == 1:
+            if opponent == 2:
+                print("You lose.")
+            elif opponent == 0:
+                print("You win!")
+            else:
+                print("Tie!")
+        elif player == 2:
+            if opponent == 0:
+                print("You lose.")
+            elif opponent == 1:
+                print("You win!")
+            else:
+                print("Tie!")
+        play_again = input("Would you like to play again? (Y/N): ").lower()
+        if play_again == "y":
+            player = int(input("What do you choose? 0 for Rock, 1 for Paper or 2 for Scissors: "))
         else:
-            print("Tie!")
-    elif player == 1:
-        if opponent == 2:
-            print("You lose.")
-        elif opponent == 0:
-            print("You win!")
-        else:
-            print("Tie!")
-    elif player == 2:
-        if opponent == 0:
-            print("You lose.")
-        elif opponent == 1:
-            print("You win!")
-        else:
-            print("Tie!")
-    play_again = input("Would you like to play again? (Yes/No): ").lower()
-    if play_again == "yes":
-        player = int(input("What do you choose? 0 for Rock, 1 for Paper or 2 for Scissors: "))
+            print("Bye!")
+            break
     else:
-        print("Bye!")
-        break
-
-
-
-else:
-    print("Invalid choice. Choose again.")
+        print("Invalid choice. Choose again.")
+        player = int(input("What do you choose? 0 for Rock, 1 for Paper or 2 for Scissors: "))
