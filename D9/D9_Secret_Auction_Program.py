7ffb296dc75fb4494fb6882897bf8e6ca549c0c3
Nyunt Tin <nyunt.tin4303@gmail.com>
2025-02-10 23:43:07 +0700

    change dictionary name and add welcome

--- D9/D9_Secret_Auction_Program.py
+++ D9/D9_Secret_Auction_Program.py
@@ -11,22 +11,23 @@ logo = r'''
                        .-------------.
                       /_______________\\
 '''
-dict = {}
+bids = {}
+print("Welcome to the Secret Auction Program.")
 print(logo)
 loop = True
 while loop:
     name = input("What is your name?: ")
     bid = int(input("What is your bid?: $"))
-    dict[name] = bid
+    bids[name] = bid
     q = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
     if q == "no":
         person = ""
         maxNum = 0
-        for i in dict:
-            if dict[i] > maxNum:
-                maxNum = dict[i]
+        for i in bids:
+            if bids[i] > maxNum:
+                maxNum = bids[i]
                 person = i
-        print(f"The winner is {person} with a bid of ${dict[person]}.")
+        print(f"The winner is {person} with a bid of ${bids[person]}.")
         break
     else:
         print("\n"*100)
