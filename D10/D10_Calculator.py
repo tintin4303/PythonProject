242e9e4e2b2c30e1b2ee77a477924d6f5ea392cc
Nyunt Tin <nyunt.tin4303@gmail.com>
2025-03-23 04:23:40 +0700

    add error handling

--- D10/D10_Calculator.py
+++ D10/D10_Calculator.py
@@ -45,17 +45,34 @@ while True:
     | |___|___|___| |___| |
     |_____________________|
     """)
-    first_num = float(input("What's the first number?: "))
+    try:
+        first_num = float(input("What's the first number?: "))
+    except ValueError:
+        print("Invalid input! Enter a digit.")
+        first_num = float(input("What's the first number?: "))
     cont = 'y'
     while cont == 'y':
         print("+\n-\n*\n/\n^")
         operation = input("Pick an operation: ")
+        while operation not in operations:
+            print("Choose from the provided list of operations.")
+            print("+\n-\n*\n/\n^")
+            operation = input("Pick an operation: ")
+
         next_num = float(input("What's the next number?: "))
+
         result = operations[operation](first_num, next_num)
         print(f"{first_num} {operation} {next_num} = {result}")
         cont = input(f"Type 'y' to continue calculating with {result}, or Type 'n' to start a new calculation: ").lower()
+        while cont != 'y' and cont != 'n':
+            print("Invalid input! Enter 'y' or 'n'")
+            cont = input(f"Type 'y' to continue calculating with {result}, or Type 'n' to start a new calculation: ").lower()
         if cont == 'y':
             first_num = result
+
     end = input("Would you like to continue using the calculator?(Y/N): ").lower()
+    while end != 'y' and end != 'n':
+        print("Invalid input! Enter 'Y' or 'N'")
+        end = input("Would you like to continue using the calculator?(Y/N): ").lower()
     if end == 'n':
         break
